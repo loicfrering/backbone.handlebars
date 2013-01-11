0fd23177be93eacb8122677b182b70998e9b397e
Loïc Frering <loic.frering@gmail.com>
2013-01-11 23:12:33 +0100

    Nested views can now be lazily required by requirejs.

--- src/helpers/view.js
+++ src/helpers/view.js
@@ -1,5 +1,18 @@
 Handlebars.registerHelper('view', function(name, options) {
-  var view = new window[name](options.hash);
+  var viewClass;
+  resolveViewClass(name, function(resolvedViewClass) {
+    viewClass = resolvedViewClass;
+  });
+
+  var view = new viewClass(options.hash);
   view.render();
   return new Handlebars.SafeString(view.$el[0].outerHTML);
 });
+
+function resolveViewClass(name, callback) {
+  if (window[name]) {
+    callback(window[name]);
+  } else if (typeof require !== 'undefined') {
+    require(name, callback);
+  }
+}
--- test/helpers/view.js
+++ test/helpers/view.js
@@ -1,32 +1,64 @@
-window.MyView = null;
+window.MyView  = undefined;
+window.require = undefined;
 
 describe('Helpers', function() {
 
   describe('view helper', function() {
 
-    it('should render a nested view', function() {
-      window.MyView = Backbone.View.extend({
-        render: function() {
-          this.$el.html('Hello World!');
-        }
+    describe('nested view', function() {
+
+      it('should render a nested view defined in the window global object', function() {
+        window.MyView = Backbone.View.extend({
+          render: function() {
+            this.$el.html('Hello World!');
+          }
+        });
+
+        var fn = Handlebars.compile('{{view "MyView"}}');
+        fn().should.equal('<div>Hello World!</div>');
+
+        window.MyView = undefined;
       });
 
-      var fn = Handlebars.compile('{{view "MyView"}}');
-      fn().should.equal('<div>Hello World!</div>');
+      it('should pass helper\'s options to the nested view', function() {
+        window.MyView = Backbone.View.extend({
+          initialize: function() {
+            this.options.should.deep.equal({foo: 'bar', name: 'World'});
+          },
+          render: function() {
+            this.$el.html('Hello ' + this.options.name + '!');
+          }
+        });
+
+        var fn = Handlebars.compile('{{view "MyView" name=name foo="bar"}}');
+        fn({name: 'World'}).should.equal('<div>Hello World!</div>');
+
+        window.MyView = undefined;
+      });
     });
 
-    it('should pass helper\'s options to the nested view', function() {
-      window.MyView = Backbone.View.extend({
-        initialize: function() {
-          this.options.should.deep.equal({foo: 'bar', name: 'World'});
-        },
-        render: function() {
-          this.$el.html('Hello ' + this.options.name + '!');
-        }
-      });
+    describe('view class resolution', function() {
 
-      var fn = Handlebars.compile('{{view "MyView" name=name foo="bar"}}');
-      fn({name: 'World'}).should.equal('<div>Hello World!</div>');
+      it('should require the view by its name if not found in the global window object', function() {
+        window.require = function() {
+          arguments.length.should.equal(2);
+          arguments[0].should.equal('MyViewToRequire');
+
+          var MyViewToRequire = Backbone.View.extend({
+            render: function() {
+              this.$el.html('Hello World!');
+            }
+          });
+
+          arguments[1].should.be.an.instanceOf(Function);
+          arguments[1].call(window, MyViewToRequire);
+        };
+
+        var fn = Handlebars.compile('{{view "MyViewToRequire"}}');
+        fn().should.equal('<div>Hello World!</div>');
+
+        window.require = undefined;
+      });
     });
   });
 });
